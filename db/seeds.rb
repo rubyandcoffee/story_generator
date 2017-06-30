3026bc1aad936606b034242944e02d74e8461c3a
Alexandra Wolfe <rubyandcoffee@gmail.com>
2017-06-30 15:22:53 +0100

    Add seeds for all

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,80 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# This data can be loaded with the rails db:seed command (or created alongside the database with db:setup).
+
+Character.create(
+  [
+    {
+      name: 'man',
+      character_type: 'gender'
+    },
+    {
+      name: 'woman',
+      character_type: 'gender'
+    },
+    {
+      name: 'vampire',
+      character_type: 'fantasy'
+    },
+  ]
+)
+
+Situation.create(
+  [
+    {
+      name: 'victim',
+      situation_type: 'state'
+    },
+    {
+      name: 'soldier',
+      situation_type: 'occupation'
+    },
+    {
+      name: 'witch',
+      situation_type: 'fantasy'
+    },
+  ]
+)
+
+Place.create(
+  [
+    {
+      name: 'England',
+      place_type: 'country'
+    },
+    {
+      name: 'the Vietnam War',
+      place_type: 'war'
+    },
+    {
+      name: 'a mythical world',
+      place_type: 'fantasy'
+    },
+  ]
+)
+
+FirstName.create(
+  [
+    {
+      name: 'Maria',
+    },
+    {
+      name: 'Anna',
+    },
+    {
+      name: 'Emma',
+    },
+  ]
+)
+
+LastName.create(
+  [
+    {
+      name: 'Williamson',
+    },
+    {
+      name: 'Brookes',
+    },
+    {
+      name: 'Smith',
+    },
+  ]
+)
